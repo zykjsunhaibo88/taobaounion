c083fb385954055ae2b08727e617065bffefac2d
sunhaibo <sunhaibo@tvlicai.com>
2020-07-06 07:52:44 +0800

    refs #0009 孙海波增加

--- pages/discovery/index.vue
+++ pages/discovery/index.vue
@@ -14,9 +14,13 @@
               :src="item.pict_url"></el-image>
           </div>
           <div class="item-right-info float-left">
-            <div class="item-title">
-              <span v-text="item.title"></span>
-            </div>
+
+              <div class="item-title">
+                <a :href="item.coupon_click_url" target="_blank">
+                <span v-text="item.title"></span>
+
+                </a>
+              </div>
             <div class="prise-info">
               <span class="original-prise" v-text="'原价：'+item.zk_final_price+'元'"></span>
               <span class="off-prise">券后价：<span v-text="to2Bit(item.zk_final_price-item.coupon_amount)"></span></span>
@@ -26,12 +30,26 @@
               <span>·</span>
               <span>人购买</span>
             </div>
-            <el-button class="discovery-buy-btn" type="danger" size="small">领券购买</el-button>
+            <a :href="item.coupon_click_url" target="_blank" class="discovery-buy-btn" type="danger" size="small">领券购买</a>
           </div>
         </div>
       </div>
       <div class="discovery-right-part  float-left">
-        这个是右边
+       <div class="discovery-right-loop">
+         <div class="block">
+           <span class="demonstration">默认 Hover 指示器触发</span>
+           <el-carousel
+             :autoplay="false"
+             height="150px">
+             <el-carousel-item v-for="(item,index) in loopData" :key="index">
+               <el-image
+                 style="width: 275px; height: 150px"
+                 :src="item.pict_url"
+                  fit="cover"></el-image>
+             </el-carousel-item>
+           </el-carousel>
+         </div>
+       </div>
       </div>
     </div>
   </div>
@@ -52,12 +70,14 @@
         let  recommendItem=categoriesList[0];
         let  currentCategoryId=recommendItem.id;
         let  contentResultData = await api.getCategoriesContext(currentCategoryId,1);
+        let  loopData=contentResultData.data.slice(0,3)
         if(contentResultData.code===api.SUCCESS_CODE){
 
           return{
             categoriesList,
             currentCategoryId,
-            contentList:contentResultData.data
+            contentList:contentResultData.data,
+            loopData,
           }
         }
       }
@@ -67,7 +87,8 @@
 </script>
 <style>
   .original-prise{
-
+   text-decoration: line-through;
+    color:#999999;
   }
   .off-prise{
     color: orangered;
@@ -80,8 +101,13 @@
      color: #47494e;
      font-weight: 600;
      max-width: 500px;
+     cursor: pointer;
   }
-
+  .item-title a{
+    font-size: 22px;
+    color: #47494e;
+    text-decoration: none;
+  }
   .item-left-cover img{
     border-radius: 5px;
   }
@@ -126,10 +152,10 @@
     margin-right: 10px;
 }
 .discovery-right-part{
-     width: 280px;
+     width: 275px;
      margin-left: 10px;
-     height: 500px;
-     background: #89ffaa;
+     box-shadow: 0 5px 10px #d4d4d4;
+     margin-top: 5px;
 }
 
   .prise-info{
@@ -148,5 +174,19 @@
     position: absolute;
     bottom:20px;
     right:20px;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+    text-decoration: none;
+    padding: 8px 20px;
+    border-radius: 5px;
+    color: white;
+  }
+  .discovery-buy-btn:hover{
+    background-color: orangered;
+    border-color: orangered;
+  }
+  .el-carousel__button{
+    width: 5px;
+    height: 5px;
   }
 </style>
